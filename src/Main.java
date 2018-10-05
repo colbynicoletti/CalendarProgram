539d477df0a3d274b417dd5176e426aafe71b5ef
cjn53 <cjn53@10.100.204.35>
2018-10-04 23:12:36 -0400

    Made Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		//Colby Nicoletti
-		System.out.println("Hello World");
+		System.out.println("Hellow World");
+		Scanner scan = new Scanner(System.in);
 		
+		scan.close(); 
 	}
-
 }
